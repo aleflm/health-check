eb727560a71915312afa833f4f670b37f5e97fc0
AlefLm <AlefLm@proton.me>
2025-12-19 21:53:26 +0330

    Add Firo health check

---
+++ packages/asset-check/lib/firo/types.ts
@@ -0,0 +1,12 @@
+// Firo RPC types
+export interface FiroRpcError {
+  code: number;
+  message: string;
+}
+
+export interface FiroRpcResponse<T> {
+  jsonrpc: string;
+  id: string;
+  result: T;
+  error?: FiroRpcError;
+}
